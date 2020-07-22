e6f252858394396e9aeb16da4bb88ba6023d2425
EmanuelRaileanu <raileanuemanuelalin@gmail.com>
2020-07-22 20:37:11 +0300

    updatede by EmanuelRaileanu

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import dotenv from 'dotenv';
 import path from 'path';
 import express from 'express';
-import movie_list from './movies.json';
+import Knex from 'knex';
+
+const knex = Knex({
+    client: 'mysql2',
+    connection: {
+        host: '127.0.0.1',
+        user: 'dev',
+        password: '',
+        database: 'mydb'
+    }
+});
 
 dotenv.config();
-const movies = movie_list.movies;
 
 const app = express();
 const port = process.env.SERVER_PORT;
@@ -21,8 +30,12 @@ app.get('/', (req, res) =>{
     res.send(response);
 });
 
+
 app.get('/movies', (req, res) => {
-    res.send(movie_list.movies);
+    knex.from('movies').select("*")
+    .then((rows: object[]) => {
+            res.send(rows);
+    }).catch((err) => { console.log( err); throw err });
 });
 
 app.post('/movies', (req, res) => {
@@ -30,24 +43,26 @@ app.post('/movies', (req, res) => {
         res.status(400).send('Bad request');
         return;
     }
-    const movie = {
-        id: movies.length + 1,
+
+    const movies = {
         name: req.body.name
     };
-    movie_list.movies.push(movie);
+    console.log(movies);
+    knex('movies').insert(movies)
+    .then(() => console.log("data inserted"))
+    .catch((err) => { console.log(err); throw err });
     res.send('POST request received');
 });
 
-app.delete('/movies/:id', (req, res) => {
+/*app.delete('/movies/:id', (req, res) => {
     const movie = movies.find(m => m.id === parseInt(req.params.id, 10));
     if(!movie){ res.status(404).send('Movie not found'); return; }
     else movies.splice(parseInt(req.params.id, 10) - 1, 1);
     for(let i = parseInt(req.params.id, 10) - 1; i < movies.length; i++)
         movies[i].id--;
     res.send('DELETE request received');
-});
+});*/
 
 app.listen(port, () => {
-    // tslint:disable-next-line:no-console
     console.log(`Server started on port ${port}...`);
 });
