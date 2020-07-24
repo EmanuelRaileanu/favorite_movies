63e551fdcfbf5e4efd0720a278a14ad10263cea3
EmanuelRaileanu <raileanuemanuelalin@gmail.com>
2020-07-24 10:51:13 +0300

    update

--- src/migrations/20200724103555_movies.js
+++ src/migrations/20200724103555_movies.js
@@ -8,16 +8,9 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-    return knex.schema.dropTable('movies', table => {
-        table.dropColumn('id');
-        table.dropColumn('title');
-        table.dropColumn('description');
-        table.dropColumn('runtime');
+    return knex.schema.alterTable('movies', table => {
         table.dropColumn('releaseDate');
         table.dropColumn('budget');
         table.dropColumn('gross');
-        table.dropColumn('overallRating', 2, 1);
-        table.dropTimestamps('createdAt');
-        table.dropTimestamps('updatedAt');
     })
 };
