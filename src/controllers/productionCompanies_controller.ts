0f3c2c77d420ab2f732909aa299cb5f05443b7b4
EmanuelRaileanu <raileanuemanuelalin@gmail.com>
2020-07-28 17:36:56 +0300

    Moved 'paginate.ts' in 'src/utilities'

--- src/controllers/productionCompanies_controller.ts
+++ src/controllers/productionCompanies_controller.ts
@@ -2,14 +2,24 @@ import dotenv from 'dotenv';
 import Knex from 'knex';
 import * as config from '../../knexfile.js';
 import express from 'express';
-import { paginate } from './paginate';
+import { paginate } from '../utilities/paginate';
 
 dotenv.config();
 
 const knex = Knex(config.development);
 
 export const getProductionCompanies = async (req: express.Request, res: express.Response) => {
-    paginate(req, res, 'production_companies');
+    const reg = new RegExp('^[0-9]$');
+    const page = parseInt(reg.test(String(req.query.page))? String(req.query.page) : '1', 10) || 1;
+    const pageSize = parseInt(reg.test(String(req.query.pageSize))? String(req.query.pageSize) : String(length), 10) || length;
+    const result = await paginate('production_companies', page, pageSize);
+
+    if(parseInt(String(result), 10) === 404){
+        res.status(404).send('Page not found');
+        return;
+    }
+
+    res.send(result);
 };
 
 export const getProductionCompanyById = async (req: express.Request, res: express.Response) => {
--- src/utilities/paginate.ts
+++ src/utilities/paginate.ts
@@ -1,23 +1,18 @@
 import dotenv from 'dotenv';
 import Knex from 'knex';
 import * as config from '../../knexfile.js';
-import express from 'express';
 
 dotenv.config();
 
 const knex = Knex(config.development);
 
-export const paginate = async (req: express.Request, res: express.Response, table: string) => {
-    const reg = new RegExp('^[0-9]$');
+export const paginate = async (table: string, page: number, pageSize: number) => {
     const length = parseInt(String((await knex(table).count('id'))[0]['count(`id`)']), 10);
-    const page = parseInt(reg.test(String(req.query.page))? String(req.query.page) : '1', 10) || 1;
-    const pageSize = parseInt(reg.test(String(req.query.pageSize))? String(req.query.pageSize) : String(length), 10) || length;
     const pageCount = Math.ceil(length / pageSize);
     const rows = await knex.from(table).select("*").offset((page - 1) * pageSize).limit(pageSize);
 
     if(page > pageCount || page <= 0){
-        res.status(404).send('Page not found');
-        return;
+        return 404;
     }
 
     const pagination = {
@@ -26,8 +21,8 @@ export const paginate = async (req: express.Request, res: express.Response, tabl
         pageCount
     };
 
-    res.send({
+    return {
         pagination,
         results: rows
-    });
+    };
 };
