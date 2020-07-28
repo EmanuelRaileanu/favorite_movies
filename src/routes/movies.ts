dc38e5f2c444c2d457700d88d11f83e692729b9f
EmanuelRaileanu <raileanuemanuelalin@gmail.com>
2020-07-28 14:31:42 +0300

    Small improvements

--- src/routes/movies.ts
+++ src/routes/movies.ts
@@ -2,21 +2,11 @@ import express from 'express';
 import * as controller from '../controllers/movies_controller';
 
 export const register = (app: express.Application) => {
+    app.get('/movies', controller.getMovies);
 
-    app.get('/movies', (req, res) => {
-        controller.getMovies(req, res);
-    });
+    app.get('/movies/:id', controller.getMovieById);
 
-    app.get('/movies/:id', (req, res) => {
-        controller.getMovieById(req, res);
-    });
+    app.post('/movies', controller.postMovie);
 
-    app.post('/movies', (req, res) => {
-        controller.postMovie(req, res);
-    });
-
-    app.delete('/movies/:id', (req, res) => {
-        controller.deleteMovie(req, res);
-    });
-
+    app.delete('/movies/:id', controller.deleteMovie);
 };
--- src/routes/productionCompanies.ts
+++ src/routes/productionCompanies.ts
@@ -2,21 +2,11 @@ import express from 'express';
 import * as controller from '../controllers/productionCompanies_controller'
 
 export const register = (app: express.Application) => {
+    app.get('/productionCompanies', controller.getProductionCompanies);
 
-    app.get('/productionCompanies', (req, res) => {
-        controller.getProductionCompanies(req, res);
-    });
+    app.get('/productionCompanies/:id', controller.getProductionCompanyById);
 
-    app.get('/productionCompanies/:id', (req, res) => {
-        controller.getProductionCompanyById(req, res);
-    });
+    app.post('/productionCompanies', controller.postProductionCompany);
 
-    app.post('/productionCompanies', (req, res) => {
-        controller.postProductionCompany(req, res);
-    });
-
-    app.delete('/productionCompanies/:id', (req, res) => {
-        controller.deleteProductionCompany(req, res);
-    });
-
+    app.delete('/productionCompanies/:id', controller.deleteProductionCompany);
 };
