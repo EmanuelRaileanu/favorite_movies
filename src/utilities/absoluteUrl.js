e2a2cd4dbfcf2ca59e4951398b60396478106489
EmanuelRaileanu <raileanuemanuelalin@gmail.com>
2020-08-11 15:32:37 +0300

    Added a script that requests movies from omdb api and can save them locally (run with 'npm run script')

--- src/utilities/absoluteUrl.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.absoluteUrl = void 0;
-var dotenv_1 = require("dotenv");
-dotenv_1["default"].config();
-exports.absoluteUrl = "http://" + process.env.API_URL + ":" + process.env.SERVER_PORT + "/";
